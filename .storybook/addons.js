2cbf5b9c9723c0ca782124436126089589ec3175
Ben Rogerson <info@benrogerson.com.au>
2020-02-23 12:49:08 +1030

    updates

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -3,12 +3,12 @@
 
 // Interact with component inputs dynamically in the Storybook UI
 // https://github.com/storybooks/storybook/tree/master/addons/knobs
-import "@storybook/addon-knobs/register";
+import '@storybook/addon-knobs/register'
 
 // Test component compliance with web accessibility standards
 // https://github.com/storybooks/storybook/tree/master/addons/a11y
-import "@storybook/addon-a11y/register";
+import '@storybook/addon-a11y/register'
 
 // Build responsive components by adjusting Storybook’s viewport size and orientation
 // https://github.com/storybooks/storybook/tree/master/addons/viewport
-import '@storybook/addon-viewport/register';
+import '@storybook/addon-viewport/register'
